bc2ac8eaf26cccaf68020c595c16e0298ccad966
Asura <todaymofish@gmail.com>
2023-04-21 00:35:09 +0800

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Mac on 2023/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
