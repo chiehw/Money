62929f34f36638bf925addf0b7dff5565a873c7a
Asura <todaymofish@gmail.com>
2023-04-22 11:39:15 +0800

    fix: store tasks

--- Money/UserDefaultValue.swift
+++ Money/UserDefaultValue.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
+@propertyWrapper
 struct UserDefaultValue<Value: Codable> {
     let key: String
     let defaultValue: Value
     
-    var value: Value {
+    var wrappedValue: Value {
         get {
             let data = UserDefaults.standard.data(forKey: key)
             let value = data.flatMap{ try? JSONDecoder().decode(Value.self, from: $0) }
